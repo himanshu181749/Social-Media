5e0aa742caa24ba762bf7e0a9f9d659b0ccdceb3
Himanshu Gupta <75875283+himanshu181749@users.noreply.github.com>
2024-10-03 22:08:57 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/miniproject');
+// mongoose.connect('mongodb://127.0.0.1:27017/miniproject');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
