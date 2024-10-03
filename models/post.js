ddf6e623892b310a69afae015d194b2aeea8465e
Himanshu Gupta <75875283+himanshu181749@users.noreply.github.com>
2024-10-03 22:08:34 +0530

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/miniproject');
+// mongoose.connect('mongodb://127.0.0.1:27017/miniproject');
 
 // const PostSchema = new mongoose.Schema({
 // title: { type: String, required: true },
